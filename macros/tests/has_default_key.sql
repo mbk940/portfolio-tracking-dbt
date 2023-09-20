a5b46f6da52253262f98a221a3f4011ef007d866
mbk.snowflake@gmail.com <mbk.snowflake@gmail.com>
2023-09-20 10:43:56 +0000

    define tests

---
+++ macros/tests/has_default_key.sql
@@ -0,0 +1,27 @@
+{% test has_default_key(
+    model
+    , column_name
+    , default_key_value = '-1'
+    , record_source_field_name = 'RECORD_SOURCE'
+    , default_key_record_source = 'System.DefaultKey'
+) -%}
+{{ config(severity ='error') }}
+
+WITH 
+default_key_rows AS (
+    SELECT DISTINCT {{column_name}}, {{ record_source_field_name }}
+    FROM {{ model }}
+    WHERE {{ column_name }} = '{{ default_key_value }}'
+        AND {{ record_source_field_name }} = '{{ default_key_value }}'
+),
+validation_errors AS (
+    SELECT '{{ default_key_value }}' AS {{ column_name }}
+            , '{{ default_key_record_source }}' AS {{ record_source_field_name }}
+    EXCEPT
+    SELECT {{ column_name }}, {{ record_source_field_name }}
+    FROM default_key_rows
+)
+SELECT * FROM validation_errors
+
+{%- endtest %}
+
